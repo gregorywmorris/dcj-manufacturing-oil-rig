6bfe76ca8ed578d683c5efd1f37d5fa5336d9e1b
gregorywmorris <gwm1980@gmail.com>
2023-05-31 19:07:06 -0400

    dir update

--- upload-aws.py
+++ upload-aws.py
@@ -1,17 +1,36 @@
 import boto3
-import pandas as pd
 import os
 import ignore.secrets as secrets
 
-os.environ["AWS_DEFAULT_REGION"] = 'us-east-2'
-os.environ["AWS_ACCESS_KEY_ID"] = secrets.AWS_ACCESS_KEY_ID
-os.environ["AWS_SECRET_ACCESS_KEY"] = secrets.AWS_SECRET_ACCESS_KEY
 
-csv_directory = './data/'
-target_bucket = 'well-logs'
+# establish a connection
+s3 = boto3.client(
+    's3', 
+    region_name='us-east-1', 
+    aws_access_key_id=secrets.AWS_ACCESS_KEY_ID, 
+    aws_secret_access_key=secrets.AWS_SECRET_ACCESS_KEY
+)
+
+#Create bucket
+bucket_name = 'well-logs'
+
+response = s3.create_bucket(
+    Bucket=bucket_name,
+)
+
+print(response)
+
+# upload files
+csv_directory = './logs/'
 for filename in os.listdir(csv_directory):
-    s3.Bucket(target_bucket).upload_file(Filename=filename, Key=filename)
+    file_path = os.path.join(csv_directory, filename)
+    s3.upload_file(file_path, bucket_name, filename)
 
 # Confirm successful upload
-for obj in s3.Bucket(target_bucket).objects.all():
-    print(obj)
+response = s3.list_objects(Bucket=bucket_name)
+objects = response.get('Contents', [])
+if objects:
+    for obj in objects:
+        print(obj['Key'])
+else:
+    print("No objects found in the bucket.")
