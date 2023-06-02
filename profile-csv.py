a575e68d5d0902ca84e05be31d96f47b9cac5460
gregorywmorris <gwm1980@gmail.com>
2023-06-02 19:51:58 -0400

    new profile py

--- profile-csv.py
+++ profile-csv.py
@@ -1,12 +1,14 @@
+import pandas as pd
+import os
 
+data_location = r'./outcomes/'
 
+for files in os.listdir(path=data_location):
+    file_name, file_ext = os.path.splitext(files)
+    df = pd.read_table(f'./outcomes/{files}', index_col=False, delim_whitespace=True, header=None)
+    profile_columns = ['cooler_condition', 'valve_condition', 'internal_pump_leakage', 'hydrolic_acucumlator_bar', 'stable_flag']
+    df.columns = profile_columns
 
-# oil well condition
-        profile_df = pd.read_table(r'./outcomes/profile.txt', index_col=False, delim_whitespace=True,header=None)
-
-        rofile_columns = ['cooler_condition','valve_condition','internal_pump_leakage','hydrolic_acucumlator_bar','stable_flag']
-
-        profile_df.columns = profile_columns
-
-        for c in profile_columns:
-            df[c]= profile_df[c].values
+    # export
+    csv_filename = f'{file_name}.csv'
+    df.to_csv(r'./outcomes/' + csv_filename, index=False)
