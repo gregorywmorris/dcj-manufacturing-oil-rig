938522e433873da4e06798ba50c446eea6378f0b
gregorywmorris <gwm1980@gmail.com>
2023-06-02 15:55:44 -0400

    readme, sql

--- README.md
+++ README.md
@@ -25,11 +25,11 @@ Oil wells can make $500,000+ / day, so these machines are highly valuable. Makin
 * Data 
     * Used internally and does not meet the requirements of big data. 
     * Currently in table format, whitespace deliminated.
-    * Data is provided in 3 month batches.
+    * Data is provided in 3 month batches/cohorts.
     * Data scientists need access to csv's in AWS S3.
     * SOLUTION: Use current onsite MySQL database and S3 bucket.
 * Database schema
-    * [Figjam](https://www.figma.com/file/wblGp1sxj3uJhKLCxmVHuY/energymobile---database-design?type=whiteboard&node-id=0-1&t=oId6SLpAnpLL8sIR-0) 
+    * [Figjam](https://www.figma.com/file/wblGp1sxj3uJhKLCxmVHuY/energymobile---database-design?type=whiteboard&node-id=0-1&t=oId6SLpAnpLL8sIR-0) (see below)
 * Process flow
     * Inital: Manual batch processing to get intial data into production.
     * Futurestate: Schedule processing at the end of every chort. Additional clarification needed from operations.
@@ -38,3 +38,5 @@ Oil wells can make $500,000+ / day, so these machines are highly valuable. Makin
         * Can format or information be modified by sending system?
 
 ![system design](images/system-design.jpg)
+
+![database design](images/db-design.jpg)
--- db_creation.sql
+++ db_creation.sql
@@ -1,35 +1,67 @@
 
-CREATE DATABASE energymobile;
+IF NOT EXISTS CREATE DATABASE energymobile;
 
 use energymobile;
 
-DROP TABLE `wellTests`;
+IF EXISTS DROP TABLE  `sensors`,'cooler_condition','valve_condition','pump_leakage','hydraulic_accumulator','stable_flag','sensor_data';
 
-CREATE TABLE `wellTests` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `ce` float,
-  `cp` float,
-  `eps1` float,
-  `fs1` float,
-  `fs2` float,
-  `ps1` float,
-  `ps2` float,
-  `ps3` float,
-  `ps4` float,
-  `ps5` float,
-  `ps6` float,
-  `se` float,
-  `ts1` float,
-  `ts2` float,
-  `ts3` float,
-  `ts4` float,
-  `vs1` float,
-  `cumulative_minutes` integer,
-  `cohort` integer,
-  `date_time` datetime,
-  `cooler_condition` integer,
-  `valve_condition` integer,
-  `internal_pump_leakage` integer,
-  `hydrolic_acucumlator_bar` integer,
-  `stable_flag` integer
-);
+CREATE TABLE sensors (
+  sensor_name varchar not null,
+  physical_quantity varchar not null,
+  unit varchar not null,
+  sampling_rate varchar not null,
+  primary key (sensor_name)
+)
+
+CREATE TABLE cooler_condition (
+  id int not null,
+  condition_percentage float not null,
+  cooler_description varchar not null,
+  primary key (id)
+)
+
+CREATE TABLE valve_condition (
+  id int not null,
+  condition_percentage float not null,
+  valve_description varchar not null,
+  primary key (id)
+)
+
+CREATE TABLE pump_leakage (
+  leakage_level int not null,
+  pump_description varchar not null,
+  primary key (leakage_level)
+)
+
+CREATE TABLE hydraulic_accumulator (
+  pressure_bar  int not null,
+  hydraulic_description varchar not null,
+  primary key (pressure_bar)
+)
+
+CREATE TABLE stable_flag (
+  flag_value int not null,
+  flag_description varchar not null,
+  primary key (flag_value)
+)
+
+CREATE TABLE sensor_data (
+  data_id int not null auto_increment,
+  sensor_name varchar
+  time_stamp datetime,
+  cumulative_minutes int,
+  cohort int,
+  sensor_value int,
+  cooler_condition int,
+  valve_condition int,
+  pump_leakage int,
+  hydraulic_accumulator int,
+  stable_flag int
+  primary key (data_id)
+  FOREIGN KEY (sensor_name) REFERENCES sensors(sensor_name)
+  FOREIGN KEY (cooler_condition) REFERENCES cooler_condition(cooler_id)
+  FOREIGN KEY (valve_condition) REFERENCES valve_condition(valve_id)
+  FOREIGN KEY (pump_leakage) REFERENCES pump_leakage(leakage_level)
+  FOREIGN KEY (hydraulic_accumulator) REFERENCES hydraulic_accumulator(pressure_bar)
+  FOREIGN KEY (stable_flag) REFERENCES stable_flag(flag_value)
+)
