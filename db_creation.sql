542758c524e5e8af0c25467a4591099b5cb7a702
gregorywmorris <gwm1980@gmail.com>
2023-06-02 22:39:37 -0400

    gitignor sql py readme

--- db_creation.sql
+++ db_creation.sql
@@ -1,12 +1,12 @@
-IF NOT EXISTS CREATE DATABASE energymobile;
+CREATE DATABASE IF NOT EXISTS energymobile;
 
 USE energymobile;
 
 DROP TABLE IF EXISTS sensors;
 DROP TABLE IF EXISTS cooler_condition;
 DROP TABLE IF EXISTS valve_condition;
-DROP TABLE IF EXISTS pump_leakage;
-DROP TABLE IF EXISTS hydraulic_accumulator;
+DROP TABLE IF EXISTS internal_pump_leakage;
+DROP TABLE IF EXISTS hydraulic_accumulator_bar;
 DROP TABLE IF EXISTS stable_flag;
 DROP TABLE IF EXISTS condition;
 DROP TABLE IF EXISTS sensor_data;
@@ -15,7 +15,7 @@ CREATE TABLE sensors (
   sensor_name varchar(255) not null,
   physical_quantity varchar(255) not null,
   unit varchar(255) not null,
-  sampling_rate varchar(255) not null,
+  sampling_rate_hz int not null,
   PRIMARY KEY (sensor_name)
 );
 
@@ -33,13 +33,13 @@ CREATE TABLE valve_condition (
   PRIMARY KEY (valve_id)
 );
 
-CREATE TABLE pump_leakage (
+CREATE TABLE internal_pump_leakage (
   leakage_level int not null,
   pump_description varchar(255) not null,
   PRIMARY KEY (leakage_level)
 );
 
-CREATE TABLE hydraulic_accumulator (
+CREATE TABLE hydraulic_accumulator_bar (
   pressure_bar int not null,
   hydraulic_description varchar(255) not null,
   PRIMARY KEY (pressure_bar)
@@ -55,14 +55,14 @@ CREATE TABLE profile (
   profile_id int not null auto_increment,
   cooler_condition int,
   valve_condition int,
-  pump_leakage int,
-  hydraulic_accumulator int,
+  internal_pump_leakage int,
+  hydraulic_accumulator_bar int,
   stable_flag int,
   PRIMARY KEY (profile_id),
   FOREIGN KEY (cooler_condition) REFERENCES cooler_condition(cooler_id),
   FOREIGN KEY (valve_condition) REFERENCES valve_condition(valve_id),
-  FOREIGN KEY (pump_leakage) REFERENCES pump_leakage(leakage_level),
-  FOREIGN KEY (hydraulic_accumulator) REFERENCES hydraulic_accumulator(pressure_bar),
+  FOREIGN KEY (internal_pump_leakage) REFERENCES internal_pump_leakage(leakage_level),
+  FOREIGN KEY (hydraulic_accumulator_bar) REFERENCES hydraulic_accumulator_bar(pressure_bar),
   FOREIGN KEY (stable_flag) REFERENCES stable_flag(flag_value)
 );
 
@@ -78,3 +78,37 @@ CREATE TABLE sensor_data (
   FOREIGN KEY (sensor_name) REFERENCES sensors(sensor_name),
   FOREIGN KEY (condition) REFERENCES condition(condition_id)
 );
+
+INSERT INTO cooler_condition
+VALUES (3, 0.03, 'close to total failure'), (20, 0.20, 'reduced efficiency'), (100, 1.00, 'close to total failure');
+
+INSERT INTO valve_condition
+VALUES (100, 1.00, 'optimal switching behavior'), (90, 0.90, 'small lag'), (80, 0.80, 'severe lag'), (73, 0.73, 'closer to total failure');
+
+INSERT INTO internal_pump_leakage
+VALUES (0, 'no leakage'), (1, 'weak leakage'), (2, 'severe leakage');
+
+INSERT INTO hydraulic_accumulator_bar
+VALUES (130, 'optimal pressure'), (115, 'slightly reduced pressure'), (100, 'severely reduced pressure'), (90, 'close to total failure');
+
+INSERT INTO stable_flag
+VALUES (0, 'conditions were stable'), (1, 'static conditions might not have been reached yet');
+
+INSERT INTO sensors
+VALUES
+  ('PS1', 'Pressure', 'bar', 100),
+  ('PS2', 'Pressure', 'bar', 100),
+  ('PS3', 'Pressure', 'bar', 100),
+  ('PS4', 'Pressure', 'bar', 100),
+  ('PS5', 'Pressure', 'bar', 100),
+  ('PS6', 'Pressure', 'bar', 100),
+  ('FS1', 'Volume flow', 'l/min', 10),
+  ('FS2', 'Volume flow', 'l/min', 10),
+  ('TS1', 'Temperature', 'celsius', 1),
+  ('TS2', 'Temperature', 'celsius', 1),
+  ('TS3', 'Temperature', 'celsius', 1),
+  ('TS4', 'Temperature', 'celsius', 1),
+  ('VS1', 'Vibration', 'mm/s', 1),
+  ('CE', 'Cooling efficiency (virtual)', 'percent', 1),
+  ('CP', 'Cooling power (virtual)', 'kW', 1),
+  ('SE', 'Efficiency factor', 'percent', 1);
--- import-mysql.py
+++ import-mysql.py
@@ -16,9 +16,9 @@ cursor = connection.cursor()
 
 profile_directory = './profile/'
 
-for filename in os.listdir(logs_directory):
+for filename in os.listdir(profile_directory):
     if filename.endswith('.csv'):
-        csv_file_path = os.path.join(csv_directory, filename)
+        csv_file_path = os.path.join(profile_directory, filename)
         file_name, file_ext = os.path.splitext(filename)
         # Read the CSV file into a pandas DataFrame
         df = pd.read_csv(csv_file_path)
@@ -33,7 +33,7 @@ for filename in os.listdir(logs_directory):
         column_names = ', '.join(columns)
 
         # Generate the SQL query
-        sql = f"INSERT INTO sensor_data ({column_names}) VALUES ({placeholders})"
+        sql = f"INSERT INTO profile ({column_names}) VALUES ({placeholders})"
 
         # Iterate over the rows in the DataFrame
         for row in df.itertuples(index=False):
@@ -45,7 +45,7 @@ logs_directory = './logs/'
 
 for filename in os.listdir(logs_directory):
     if filename.endswith('.csv'):
-        csv_file_path = os.path.join(csv_directory, filename)
+        csv_file_path = os.path.join(logs_directory, filename)
         file_name, file_ext = os.path.splitext(filename)
         # Read the CSV file into a pandas DataFrame
         df = pd.read_csv(csv_file_path)
